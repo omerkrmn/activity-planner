b1758ddd2822f6b0156f26551947ac4eb4d1f7f6
Ömer Karaman <drakken129@gmail.com>
2025-04-30 18:20:42 +0300

    :lipstick:added activity page and route settings.

--- ActivityPlanner.Frontend/src/app/app-routing.module.ts
+++ ActivityPlanner.Frontend/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {
-    path: 'activity',
+    path: 'activity/:username/:activityname',
     component: ActivityComponent
   },{
     path: 'login',
--- ActivityPlanner.Frontend/src/app/components/activity/activity.component.ts
+++ ActivityPlanner.Frontend/src/app/components/activity/activity.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Activity } from '../../models/activity';
 import { ActivitiesService } from '../../services/activities.service';
+import { ActivatedRoute } from '@angular/router';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-activity',
   standalone: false,
   templateUrl: './activity.component.html',
-  styleUrl: './activity.component.css'
+  styleUrl: './activity.component.css',
 })
 export class ActivityComponent implements OnInit {
+  username: string = '';
+  activityname: string = '';
   activity: Activity | undefined;
-  constructor(private activityService: ActivitiesService) {
+  constructor(private activityService: ActivitiesService, private route: ActivatedRoute) {
 
   }
   ngOnInit(): void {
-    this.loadActivity()
+    this.route.paramMap.subscribe(
+      params => {
+        this.username = params.get('username') || '';
+        this.activityname = params.get('activityname') || '';
+      }
+    );
+    this.loadActivity(this.username,this.activityname);
   }
-  loadActivity() {
-    this.activityService.getOneActivity("Omer", "Test2").subscribe(res => {
+  loadActivity(username:string,activityname:string) {
+    this.activityService.getOneActivity(username,activityname ).subscribe(res => {
       this.activity = res;
     });
   }
