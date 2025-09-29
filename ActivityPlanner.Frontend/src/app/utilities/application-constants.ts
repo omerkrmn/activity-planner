c8b179a16b5124f41fff1eaba203bc55bafd4d0b
Ömer Karaman <drakken129@gmail.com>
2025-09-29 10:15:18 +0300

    :fire: Remove Angular project

--- ActivityPlanner.Frontend/src/app/utilities/application-constants.ts
+++
@@ -1,5 +0,0 @@
-export class ApplicationConstants {
-    // public static readonly Base_Url: string = "https://activityplannerapi20250501132000.azurewebsites.net/";
-    public static readonly Base_Url: string = "http://localhost:5221/";
-
-}
